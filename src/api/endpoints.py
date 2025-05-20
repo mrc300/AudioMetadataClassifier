8ff8f0ad441fcb878013205f6373da84f00cb0a9
manuelcunha <mrffc@iscte-iul.pt>
2025-05-20 20:20:42 +0200

    Added Pydantic models for transcription response for better OpenApi spec

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, UploadFile, File
 from src.utils.audio_processing import split_audio_segments
 from src.services.transcription_service import transcribe_audio_segments
+from src.models.schema import TranscriptionResponse
 import os
 
 router = APIRouter()
 
-@router.post("/api/generate/")
+@router.post("/api/generate/" , response_model=TranscriptionResponse)
 async def transcribe_audio(file: UploadFile = File(...)):
     temp_path = f"temp_upload_{file.filename}"
     with open(temp_path, "wb") as f:
